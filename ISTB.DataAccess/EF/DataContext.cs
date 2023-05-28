1f749c3fd0c503d38c81098f09d9ccabb7c8111c
GineTik <densheva18072003@gmail.com>
2023-05-28 15:36:50 +0300

    Added base structure

---
+++ ISTB.DataAccess/EF/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ISTB.DataAccess.EF
+{
+    public class DataContext : DbContext
+    {
+        //public DbSet<User> Users { get; set; }
+        //....
+        //..
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
