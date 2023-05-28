8d560604c1ddba7dcd59d7c455b0196c188fe470
GineTik <densheva18072003@gmail.com>
2023-05-28 21:41:00 +0300

    Added first version entities

---
+++ ISTB.DataAccess/Entities/Group.cs
@@ -0,0 +1,8 @@
+﻿namespace ISTB.DataAccess.Entities
+{
+    public class Group : Entity
+    {
+        public string Name { get; set; }
+        public ICollection<Schedule> Schedules { get; set; }
+    }
+}
---
+++ ISTB.DataAccess/Entities/ScheduleDay.cs
@@ -0,0 +1,9 @@
+﻿namespace ISTB.DataAccess.Entities
+{
+    public class ScheduleDay : Entity
+    {
+        public uint Position { get; set; }
+        // image url
+        public string Description { get; set; }
+    }
+}
