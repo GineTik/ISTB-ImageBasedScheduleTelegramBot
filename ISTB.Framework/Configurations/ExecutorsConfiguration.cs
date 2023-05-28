85ad63ef6d4f6556f34e51c08752269a7647caa8
GineTik <densheva18072003@gmail.com>
2023-05-28 12:08:56 +0300

    Renamed and added executors functionality

---
+++ ISTB.Framework/Configurations/ExecutorsConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace ISTB.Framework.Configurations
+{
+    public class ExecutorsConfiguration
+    {
+        public IEnumerable<Type> ExecutorsTypes { get; set; } = new List<Type>();
+    }
+}
---
+++ ISTB.Framework/Extensions/Services/ExecutorExtensions.cs
@@ -0,0 +1,28 @@
+﻿using ISTB.Framework.Configurations;
+using ISTB.Framework.Executors;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ISTB.Framework.Extensions.Services
+{
+    public static class ExecutorExtensions
+    {
+        public static IServiceCollection AddExecutors(this IServiceCollection services, IEnumerable<Assembly>? assemblies = null)
+        {
+            assemblies ??= new List<Assembly>() { Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly() };
+
+            var configuration = new ExecutorsConfiguration()
+            {
+                ExecutorsTypes = assemblies.SelectMany(assembly => 
+                    assembly.GetTypes().Where(type => type.BaseType == typeof(Executor))
+                ),
+            };
+
+            services.AddSingleton(configuration);
+            foreach (var type in configuration.ExecutorsTypes)
+                services.AddTransient(type);
+
+            return services;
+        }
+    }
+}
