c355457b250f2a14602502ec451ea521c118e98f
GineTik <densheva18072003@gmail.com>
2023-06-10 13:49:31 +0300

    Updated TargetMethodStorage

--- ISTB.Framework/Attributes/TargetExecutorAttributes/TargetCommandsAttribute.cs
+++ ISTB.Framework/Attributes/TargetExecutorAttributes/TargetCommandsAttribute.cs
@@ -1,10 +1,12 @@
 ﻿using ISTB.Framework.Attributes.BaseAttributes;
 using System.Text.RegularExpressions;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace ISTB.Framework.Attributes.TargetExecutorAttributes
 {
-    public class TargetCommandsAttribute : TargetExecutorAttribute
+    [TargetUpdateType(UpdateType.Message)]
+    public class TargetCommandsAttribute : TargetAttribute
     {
         public string[] Commands { get; set; }
         public string Description { get; set; }
@@ -16,7 +18,7 @@ namespace ISTB.Framework.Attributes.TargetExecutorAttributes
 
         public override bool IsTarget(Update update)
         {
-            if (update?.Message?.Text is not { } text)
+            if (update.Message!.Text is not { } text)
                 return false;
 
             var command = text.Split(' ').First().TrimStart('/');
---
+++ ISTB.Framework/Attributes/TargetExecutorAttributes/TargetUpdateTypeAttribute.cs
@@ -0,0 +1,22 @@
+﻿using ISTB.Framework.Attributes.BaseAttributes;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace ISTB.Framework.Attributes.TargetExecutorAttributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class TargetUpdateTypeAttribute : TargetAttribute
+    {
+        public UpdateType UpdateType { get; set; }
+
+        public TargetUpdateTypeAttribute(UpdateType updateType)
+        {
+            UpdateType = updateType;
+        }
+
+        public override bool IsTarget(Update update)
+        {
+            return update.Type == UpdateType;
+        }
+    }
+}
